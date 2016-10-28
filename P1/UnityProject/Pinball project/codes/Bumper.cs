7aa57fa544867b981ab117d9aff20a04aca9634f
Robertdevisser-Deltion <Robert99dv@gmail.com>
2016-10-28 09:52:34 +0200

    codes / onderdelen flipperkast etc...

---
+++ P1/UnityProject/Pinball project/codes/Bumper.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Bumper : MonoBehaviour {
+
+    public Text tekstvakje;
+    public int score;
+    public Vector3 v;
+    public Rigidbody bal;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnCollisionEnter(Collision collision)
+    {
+        bal.AddForce(v);
+
+        score = score + 10;
+        tekstvakje.text = score.ToString();
+    }
+}
---
+++ P1/UnityProject/Pinball project/codes/DeathZone.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathZone : MonoBehaviour {
+
+   
+
+    void OnCollisionEnter(Collision c)
+    {
+        Destroy(c.gameObject);
+        GameObject.Find("GameManager").GetComponent<GameManager>().lives -= 1;
+    }
+}
