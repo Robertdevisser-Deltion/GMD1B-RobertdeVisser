abafa6df5e1fab2b2a5262010b35fede7e161990
Robertdevisser-Deltion <Robert99dv@gmail.com>
2017-04-13 13:32:08 +0200

    dev codes voor P2
    waren de vorige keer niet in github gezet door dat mijn github niet wou
    committen.

---
+++ P2/Source/dev/Firstcamera.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Firstcamera : MonoBehaviour
+{
+
+    public Vector3 lichaam;
+    public Vector3 camera1; 
+    public GameObject cam;
+    public GameObject body;
+    public float hoofd;
+
+
+    void Update()
+    {
+        camera1.x = -Input.GetAxis("Mouse Y");
+        {
+            cam.transform.Rotate(camera1);
+        }
+        lichaam.y = Input.GetAxis("Mouse X");
+        {
+            body.transform.Rotate(lichaam);
+        }
+    }
+
+}
---
+++ P2/Source/dev/Move.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour
+{
+
+    public Vector3 lopen;
+
+    public float hor;
+    public float ver;
+    public float speed; 
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+
+        hor = Input.GetAxis("Horizontal");
+
+        ver = Input.GetAxis("Vertical");
+
+        lopen.x = hor;
+        lopen.z = ver; 
+        transform.Translate(lopen * Time.deltaTime * speed);
+
+
+    } 
+   
+}
